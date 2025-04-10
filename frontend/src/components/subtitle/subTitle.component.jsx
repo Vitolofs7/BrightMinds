4446377451b3794a3b65a0d19d419697fa407960
FrederikWagnerC <frederikwagnercs@gmail.com>
2025-04-10 22:54:34 +0100

    fix

--- frontend/src/components/subtitle/subTitle.component.jsx
+++ frontend/src/components/subtitle/subTitle.component.jsx
@@ -3,7 +3,6 @@ import React from "react";
 
 
 export const SubTitleComponent = ({ text, boldness, size, icon, onClick, underline}) => {
-
     return (
         <SubTitleStyled className={`${boldness} ${size} ${underline} subtitle`} onClick={onClick}>
                 <h2>{text}</h2>
